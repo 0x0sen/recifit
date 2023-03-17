c584d364617548b1f2447bfc8e3d7202df1f6aa1
0x0sen <engergo94@gmail.com>
2023-03-17 11:53:32 +0900

    모든 파일 설정껍대기만 만들어서 올려놧어요 빈 파일은 깃에서 올라가지 않아서 넣어놧어요 1.진선이 형은 board000 으로 통일성있게 만들어주시고 2.지희누나는 AllRecipe000으로 통일성 있게 만들어 주세요 3.아마도 제 생각이지만 컨트롤러나 이런거는 서로 각자 만드는 부분도 있고 공통되는 부분도 있어서 서로 만든거 있으면 자세하게 설명 써서 잘사용 해봐요 4.doc 폴더에 Recipesql.txt sql 문 추가했습니다

---
+++ src/main/java/com/food/recifit/service/BoardService.java
@@ -0,0 +1,17 @@
+package com.food.recifit.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+
+
+/**
+ * 게시판
+ */
+//@Service
+//public interface BoardService {
+//
+//	
+//
+//}
---
+++ src/main/java/com/food/recifit/service/BoardServiceImpl.java
@@ -0,0 +1,19 @@
+package com.food.recifit.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+//@Slf4j
+//@Service
+//public class BoardServiceImpl implements BoardService {
+//	
+//	
+//	
+//}
