1458adf4b5b9166c9be92523645bdd8c890fbf05
Jihee Kim <applemail486@gmail.com>
2023-03-21 09:51:36 +0900

    기본 틀 생성

---
+++ src/main/java/com/food/recifit/dao/RecipeDAO.java
@@ -0,0 +1,21 @@
+package com.food.recifit.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
+
+import com.food.recifit.domain.Recipe;
+
+/**
+ *  모든 글 보기
+ * @author user
+ *
+ */
+@Mapper
+public interface RecipeDAO {
+
+	int insert(Recipe recipe);
+	
+}
